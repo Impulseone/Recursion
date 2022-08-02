e68e7b65242204dbcedfa3ce6af210ab80a86e16
Sergei <sk_developer@mail.ru>
2022-08-02 17:16:37 +0300

    Task 3 upd

--- Recursion/src/Task3.java
+++ Recursion/src/Task3.java
@@ -1,13 +1,9 @@
-import java.util.EmptyStackException;
 import java.util.Stack;
 
 public class Task3 {
     public static int pop(Stack<String> stringsStack, int index) {
-        try {
-            stringsStack.pop();
-            return pop(stringsStack, index + 1);
-        } catch (EmptyStackException e) {
-            return index;
-        }
+        if (stringsStack.empty()) return index;
+        stringsStack.pop();
+        return pop(stringsStack, index + 1);
     }
 }
