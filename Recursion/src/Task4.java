77723538b50b6f2fd428d2391e1370771b196a81
Sergei <sk_developer@mail.ru>
2022-08-03 19:22:48 +0300

    Task 4 upd

--- Recursion/src/Task4.java
+++ Recursion/src/Task4.java
@@ -1,7 +1,7 @@
 public class Task4 {
     public static boolean isPalindrom(String s) {
         boolean isFirstAndLastSymbolEquals = s.charAt(0) == s.charAt(s.length() - 1);
-        if ((s.length() == 1 || s.length() == 2) && isFirstAndLastSymbolEquals) return true;
+        if (s.length() <= 2 && isFirstAndLastSymbolEquals) return true;
         if (!isFirstAndLastSymbolEquals) return false;
         return isPalindrom(s.substring(1, s.length() - 1));
     }
