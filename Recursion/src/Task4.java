ae228478149d3a8248e40c86c1ae8d449089ea1c
Sergei <sk_developer@mail.ru>
2022-08-03 19:03:46 +0300

    Task 4 refactor

--- Recursion/src/Task4.java
+++ Recursion/src/Task4.java
@@ -1,8 +1,8 @@
 public class Task4 {
     public static boolean isPalindrom(String s) {
-        if (s.length() == 1 || (s.length() == 2 && s.charAt(0) == s.charAt(1))) return true;
-        if (s.charAt(0) == s.charAt(s.length() - 1))
-            return isPalindrom(s.substring(1, s.length() - 1));
-        return false;
+        boolean isFirstAndLastSymbolEquals = s.charAt(0) == s.charAt(s.length() - 1);
+        if ((s.length() == 1 || s.length() == 2) && isFirstAndLastSymbolEquals) return true;
+        if (!isFirstAndLastSymbolEquals) return false;
+        return isPalindrom(s.substring(1, s.length() - 1));
     }
 }
