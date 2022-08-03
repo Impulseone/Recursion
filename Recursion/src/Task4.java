4e7127dbce2415ef12eadab0bb1d30238fa5f085
Sergei <sk_developer@mail.ru>
2022-08-03 19:23:23 +0300

    Task 4 rename

--- Recursion/src/Task4.java
+++ Recursion/src/Task4.java
@@ -1,8 +1,8 @@
 public class Task4 {
     public static boolean isPalindrom(String s) {
-        boolean isFirstAndLastSymbolEquals = s.charAt(0) == s.charAt(s.length() - 1);
-        if (s.length() <= 2 && isFirstAndLastSymbolEquals) return true;
-        if (!isFirstAndLastSymbolEquals) return false;
+        boolean isFirstAndLastSymbolEqual = s.charAt(0) == s.charAt(s.length() - 1);
+        if (s.length() <= 2 && isFirstAndLastSymbolEqual) return true;
+        if (!isFirstAndLastSymbolEqual) return false;
         return isPalindrom(s.substring(1, s.length() - 1));
     }
 }
