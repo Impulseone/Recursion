47d9e0f4e021bd5c21c6ffceb2b5fe64c239a713
Sergei <sk_developer@mail.ru>
2022-08-08 19:15:28 +0300

    Task8

--- Recursion/src/Task8.java
+++ Recursion/src/Task8.java
@@ -4,6 +4,16 @@ import java.util.List;
 
 public class Task8 {
     public static List<File> findAll(File file){
-        return new ArrayList<>();
+        return find(file, new ArrayList<>());
+    }
+
+    private static List<File> find(File file, List<File> allFiles) {
+        File[] currentCatalogFiles = file.listFiles();
+        for (File currentCatalogFile : currentCatalogFiles) {
+            if (currentCatalogFile.isDirectory()) {
+                allFiles.addAll(find(currentCatalogFile, new ArrayList<>()));
+            } else allFiles.add(currentCatalogFile);
+        }
+        return allFiles;
     }
 }
