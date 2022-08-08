a911f67b1640e640acc3027258e91314b13e9b26
Sergei <sk_developer@mail.ru>
2022-08-08 16:27:05 +0300

    upd4

--- Recursion/src/Task7.java
+++ Recursion/src/Task7.java
@@ -2,14 +2,20 @@ import java.util.List;
 
 public class Task7 {
     public static int findSecondMax(List<Integer> list) {
-        return findMax(list, 0, 0);
+        return findMax(list, list.get(0), null, 1);
     }
 
-    private static int findMax(List<Integer> list, int max, int index) {
-        if (index == list.size()) return max;
-        if (list.get(index) > max) {
-            max = list.get(index);
+    private static int findMax(List<Integer> all, Integer max, Integer secondMax, int index) {
+        if (index == all.size()) {
+            return secondMax == null ? max : secondMax;
         }
-        return findMax(list, max, index + 1);
+        if (all.get(index).equals(max) && secondMax == null) {
+            secondMax = all.get(index);
+        }
+        if (all.get(index) > max) {
+            max = all.get(index);
+            secondMax = null;
+        }
+        return findMax(all, max, secondMax, index + 1);
     }
 }
---
+++ Recursion/src/Task8.java
@@ -0,0 +1,9 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Task8 {
+    public static List<File> findAll(File file){
+        return new ArrayList<>();
+    }
+}
